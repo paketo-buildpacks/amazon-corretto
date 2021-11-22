b17b0a3af6ed2b4593fd25dd9f63115b2cb5abbe
David O'Sullivan <davidos@vmware.com>
2021-11-22 11:07:54 +0000

    Added buildpack.toml entries for CPEs & SBOM, bump libpak & libjvm

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -19,12 +19,19 @@ package main
 import (
 	"os"
 
+	"github.com/paketo-buildpacks/libjvm/helper"
+
 	"github.com/paketo-buildpacks/libjvm"
 	"github.com/paketo-buildpacks/libpak"
 	"github.com/paketo-buildpacks/libpak/bard"
 )
 
 func main() {
+	logger := bard.NewLogger(os.Stdout)
+
+	// not used directly, but this forces the helper module to be included in the module
+	// we need the helper module because of the way that `scripts/build/sh` builds the helper cmd
+	_ = helper.ActiveProcessorCount{Logger: logger}
 	libpak.Main(
 		libjvm.Detect{},
 		libjvm.Build{Logger: bard.NewLogger(os.Stdout)},
